ebc7666c83c4ffe9b5aaea0b2bd92582e15ca784
LZHTK <kinggusrl3@naver.com>
2024-05-14 01:13:51 +0900

    회원가입 페이지 구현

--- routes/home/index.js
+++ routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.hello); 
 router.get("/login", ctrl.output.login);
+router.get("/register",ctrl.output.register);
 router.post("/login", ctrl.process.login); 
 
   // 라우터의 역할은 해당 도메인으로 들어 왔을때 클라이언트의 요청을 연결하는 부분
