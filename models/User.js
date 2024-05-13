5026d681991cea48e9e8c8a2ef52ca88ac6d45c5
LZHTK <kinggusrl3@naver.com>
2024-05-14 01:04:01 +0900

    로그인 화면 꾸미기

--- models/User.js
+++ models/User.js
@@ -10,6 +10,9 @@ class User {
  login() {
     const body = this.body;
     const { id, psword } = UserStorage.getUserInfo(body.id);
+    // 유저가 로그인 실행시 올바른 아이디 패스워드인지 확인을 위해
+    // 클라이언트가 보낸 정보를 스토리지의 메서드로 전달하여 처리 후
+    // 클래스가 그 과정을 반환하여 받아온 후 저장한다. 
     
     if (id) {
     if ( id === body.id && psword === body.psword) {
@@ -19,6 +22,6 @@ class User {
   }
   return { success: false, msg: "존재하지 않는 아이디"};
  }
-}
+} // 그 후 위에 로직에 따라 id와 psword가 올바른지 확인한다.
 
 module.exports = User;
--- routes/home/home.ctrl.js
+++ routes/home/home.ctrl.js
@@ -17,9 +17,10 @@ const process = {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response); 
-    // 클라이언트가 전달한 데이터를 갖고 있는 user라는 인스턴스를 만듦
-    // UserStorage에서 users 데이터를 쓰기위해 불러온 부분
-    // 불러온 로그인 정보를 유저를 검증하는 로직
+    // 클라이언트가 전달한 rq 데이터를 갖고 있는 user라는 인스턴스를 만듦
+    // 해당하는 body의 내용을 user가 계속 들고 다닌다.
+    // 불러온 로그인 정보를 유저를 검증하는 로직으로 유저가 로그인 처리시 
+    // User로 넘어가서 처리
      },
 };
 // 로그인이 가능하기 위해서는 프론트에서 받은 아이디와 패스워드를 서버에서 인증해야하는데
