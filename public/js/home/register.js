c69c8a9f0f67179289db83db0e94833dc271ea67
LZHTK <kinggusrl3@naver.com>
2024-05-21 00:19:58 +0900

    회원가입 요청 구현

---
+++ public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+      name = document.querySelector("#name"),
+      psword = document.querySelector("#psword"),
+      confirmPsword = document.querySelector("#confirm-psword"),
+      // 자바스크립트에서 사용할땐 공백을 대문자로 구분 
+      registerBtn = document.querySelector("#button");
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+ const req = {
+    id: id.value,
+    name: name.value,
+    psword: psword.value,
+    confirmPsword: confirmPsword.value,
+ };
+ 
+  
+ fetch("/register", {
+   method: "POST", // 버튼이 눌릴시 해당하는 정보를 아래의 바디에 담아서 전달하기에 POST로 전달
+   headers: {
+      "CONTENT-Type": "application/json",
+   },
+   body: JSON.stringify(req),
+ }).then((res) => res.json()) // 서버로부터 응답이 오면 해당 Json 메소드를 호출해서 
+   .then((res) => {           // 서버의 응답을 다 받는 순간 promise객체를 반환하여 해당 then에 접촉
+      if(res.success) {       // 그 후 res에 접근하여 이가 true일 경우 login 페이지
+         location.href ="/login";
+      } else{
+         alert(res.msg); // 실패시 alert를 리스폰스 메시지로 띄움
+      }
+   })
+   .catch((err) => {
+      console.error(new Error("회원가입 중 에러 발생"));
+   });
+
+}
+
